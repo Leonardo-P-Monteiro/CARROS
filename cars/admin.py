3fb339eca61e218d957062cbee51d32ce331789c
Leonardo P. Monteiro <dev.pmonteiro@gmail.com>
2025-04-11 21:25:36 -0300

    section 8 - urls, views and templates
    class number 2

--- cars/admin.py
+++ cars/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Car
+from .models import Car, Brand
 
 # Register your models here.
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    search_fields = 'name',
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
     list_display = 'model', 'brand', 'factory_year', 'model_year', 'value',
-    search_fields = ('model',)
+    search_fields = ('model', 'brand')
--- cars/models.py
+++ cars/models.py
@@ -2,9 +2,33 @@ from django.db import models
 
 # Create your models here.
 
+
+
+class Brand(models.Model):
+    
+    class Meta:
+        verbose_name = 'Marca'
+        verbose_name_plural = 'Marcas'
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class Car(models.Model):
+
+    class Meta:
+        verbose_name = 'Carro'
+        verbose_name_plural = 'Carros'
+
     model = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT,
+                              related_name='car_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/%Y/%m/', blank=True, null=True) #TODO: Precisa ser feito a lógica de redução da qualidade da imagem e padronização do tamanho dela. Isso se faz nos forms antes de salvar. 
+
+    def __str__(self):
+        return self.model
