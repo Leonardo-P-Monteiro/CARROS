226fef836971f941b7e7a93dc8be783c66442229
Leonardo P. Monteiro <dev.pmonteiro@gmail.com>
2025-04-21 20:55:51 -0300

    Atualização 21/04/2025
    Ainda estamos em Forms, aula 57

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Car
+from .forms import CarForm
 
 # Create your views here.
+def cars_views(request):
+    cars = Car.objects.all()
+    search = request.GET.get('search')
+
+    if search:
+        cars = cars.filter(model__icontains=search)
+    
+    return render(request, 'cars.html', {'cars':cars})
+
+def new_car_view(request):
+    new_car_form = CarForm()
+
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+        
+        if new_car_form.is_valid():
+            new_car_form.save()
+
+    else:
+        new_car_form = CarForm()
+        
+    return render(request, 'new_car.html', {'new_car_form':new_car_form})
--- project/urls.py
+++ project/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from cars.views import cars_views, new_car_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', cars_views, name='cars_list'),
+    path('new_car/', new_car_view, name='new_car'),
 ]
 
 if settings.DEBUG:
